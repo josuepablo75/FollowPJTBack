2032fb7e1ca9e251d8cad28e2c4f698986ba15f1
Josue Pablo <ing.pablo75@gmail.com>
2020-05-04 19:19:58 -0600

    Ruta editar usuarios agregada

--- routes/user.js
+++ routes/user.js
@@ -12,5 +12,6 @@ app.put('/usuario/activar/:id', UserController.activar_estado);
 app.put('/usuario/desactivar/:id', UserController.desactivar_estado);
 app.put('/usuario/editar/imagen/:id', path, UserController.update_foto); 
 app.get('/usuario/img/:img', UserController.get_img);
+app.post('/usuario/editar/:id', UserController.editar_config);
 
 module.exports = app; 
